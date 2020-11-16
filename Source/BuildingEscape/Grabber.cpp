e4c3b05439116a903c756fb3b530fcbdfb4cfabd
Azim Momin <amomin@zynga.com>
2020-11-16 11:47:11 -0600

    Getting player view point rotation and position.

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -1,7 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
 #include "Grabber.h"
+
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -9,8 +12,6 @@ UGrabber::UGrabber()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -18,9 +19,6 @@ UGrabber::UGrabber()
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-
-	UE_LOG(LogTemp, Error, TEXT("Grabber reporting for duty."));
-	
 }
 
 
@@ -28,7 +26,17 @@ void UGrabber::BeginPlay()
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    
+    const APlayerController* playerController = GetWorld()->GetFirstPlayerController();
+    if (playerController == NULL)
+    {
+        UE_LOG(LogTemp, Error, TEXT("Could not find player controller in world. Make sure there is at least one."));
+        return;
+    }
 
-	// ...
+    FRotator playerViewPointRotation;
+    FVector playerViewPointPosition;
+    playerController->GetPlayerViewPoint(OUT playerViewPointPosition, OUT playerViewPointRotation);
+    UE_LOG(LogTemp, Log, TEXT("Position: %s Rotation: %s"), *(playerViewPointPosition.ToString()), *(playerViewPointRotation.ToString()));
 }
 
